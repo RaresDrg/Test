74308f6e84511e1304eaf1d5824f764d36861e8d
RaresDrg <dragan_rares13@yahoo.com>
2024-06-27 02:59:33 +0300

    test
    test

--- test/src/main.jsx
+++ test/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Test">
+  <BrowserRouter basename="https://raresdrg.github.io/Test/">
     <App />
   </BrowserRouter>
 );
